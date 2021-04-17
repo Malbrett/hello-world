6cd65881bb848390418e32801a317f68d5432ccf
Benjamin Hagerty <79618243+Malbrett@users.noreply.github.com>
2021-04-17 11:38:21 -0700

    Stuff from VS2019
    Uploaded some old projects

---
+++ Beepcube/Beepcube/Beepcube.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <locale>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+
+using namespace std;
+
+int stdOctave = 1;
+
+class Note {
+public:
+	const char letter[10] = { "abcdef" };
+};
+
+void playNote(char inputNote, int inputOctave)
+{
+
+}
+
+int main()
+{
+    
+}
